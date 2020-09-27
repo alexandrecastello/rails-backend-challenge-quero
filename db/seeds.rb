4eedc992ca7aceb8bbff39cabafc69295b202226
Alexandre Castello <aaca91@gmail.com>
2020-09-27 14:14:54 -0300

    seed setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Seeding Universities
+['UNIP', 'UNINOVE', 'UNICSUL', 'MACKENZIE', 'MAUÁ', 'FMU'].each do |university|
+  University.create name: university, score: ((0..50).to_a.sample.to_f)/10, logo_url: 'https://picsum.photos/200/200'
+end
+
+# Seeding Campi
+20.times do
+  Campus.create name: Faker::Address.unique.street_name, city: Faker::Address.unique.city 
+end
+
+#Seeding Courses
+15.times do
+  new_course = Courses.new name: Faker::Educator.subject
+  new_course.kind = ['Presencial', 'EaD'].sample
+  new_course.level = ['Bacharelado', 'Tecnólogo', 'Mestrado', 'Doutorado'].sample
+  new_course.shift = new_course.kind == 'EaD' ? 'Virtual' : ['Diurno', 'Vespertino', 'Noturno'].sample
+  new_course.university = University.all.sample
+  new_course.campus = Campus.all.sample
+  new_course.save
+end
+
+# Seeding offers
+new_offer = Offer.
+
+Faker::UniqueGenerator.clear # Clears used values for all generators
