adb3df3705fdc257a341d4945e14893f1376f388
Alexandre Castello <aaca91@gmail.com>
2020-09-28 23:33:13 -0300

    multifilter implemented

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::CoursesController < Api::V1::BaseController
   def index
-    if params[:query].present?
-      sql_query = "courses.kind @@ :query \
-        OR courses.level @@ :query \
-        OR courses.shift @@ :query \
-        OR universities.name @@ :query\
-        "
-      @courses = Course.joins(:university).where(sql_query, query: "%#{params[:query]}%")
-    else
-      @courses = Course.all
-    end
+    @courses = Course.joins(:university)
+    # @courses = @courses.where("courses.name @@ :query", query: "%#{params[:course_name]}%") if params[:course_name]
+    @courses = @courses.where("courses.level @@ :query", query: "%#{params[:course_level]}%") if params[:course_level]
+    @courses = @courses.where("courses.kind @@ :query", query: "%#{params[:course_kind]}%") if params[:course_kind]
+    @courses = @courses.where("courses.shift @@ :query", query: "%#{params[:course_shift]}%") if params[:course_shift]
+    @courses = @courses.where("universities.name @@ :query", query: "%#{params[:university_name]}%") if params[:university_name]
   end
 end
--- app/controllers/api/v1/offers_controller.rb
+++ app/controllers/api/v1/offers_controller.rb
@@ -1,17 +1,12 @@
 class Api::V1::OffersController < Api::V1::BaseController
   def index
-    if params[:query].present?
-      sql_query = "courses.kind @@ :query \
-        OR courses.name @@ :query \
-        OR courses.level @@ :query \
-        OR courses.shift @@ :query \
-        OR universities.name @@ :query\
-        OR campus.city @@ :query\
-        "
-      @offers = Offer.joins(course: %i[:university campus]).where(sql_query, query: "%#{params[:query]}%")
-    else
-      @offers = Offer.all
-    end
-    @offers.order!(price_with_discount: params[:order].to_sym) if params[:order].present?
+    @offers = Offer.joins(course: %i[university campus])
+    @offers = @offers.where("courses.name @@ :query", query: "%#{params[:course_name]}%") if params[:course_name]
+    @offers = @offers.where("courses.level @@ :query", query: "%#{params[:course_level]}%") if params[:course_level]
+    @offers = @offers.where("courses.kind @@ :query", query: "%#{params[:course_kind]}%") if params[:course_kind]
+    @offers = @offers.where("courses.shift @@ :query", query: "%#{params[:course_shift]}%") if params[:course_shift]
+    @offers = @offers.where("universities.name @@ :query", query: "%#{params[:university_name]}%") if params[:university_name]
+    @offers = @offers.where("campus.city @@ :query", query: "%#{params[:campus_city]}%") if params[:campus_city]
+    @offers.order!(price_with_discount: params[:order].to_sym) if params[:order]
   end
 end
